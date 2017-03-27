90171ce1ce25212b428ee94fa98162575aba1080
Tomás Carvalho <tomcarvalhoc12@gmail.com>
2017-03-27 11:19:52 +0100

    Add sitemap

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,10 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/sitemap.xml')
+def static_from_root():
+    return send_from_directory(app.static_folder, request.path[1:])
 
 
 if __name__ == '__main__':
